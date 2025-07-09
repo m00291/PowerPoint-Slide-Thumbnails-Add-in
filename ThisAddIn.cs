b65242c22b0299e94bc70267a11f6933d080c588
m00291 <62349178+m00291@users.noreply.github.com>
2025-07-10 00:24:37 +0800

    ver 0

---
+++ ThisAddIn.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.Office.Core;
+using Microsoft.Office.Interop.PowerPoint;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Xml.Linq;
+using Office = Microsoft.Office.Core;
+using PowerPoint = Microsoft.Office.Interop.PowerPoint;
+
+namespace PowerPointSlideThumbnailsAddIn
+{
+    public partial class ThisAddIn
+    {
+
+        private void ThisAddIn_Startup(object sender, System.EventArgs e)
+        {
+        }
+
+        private void ThisAddIn_Shutdown(object sender, System.EventArgs e)
+        {
+        }
+
+        #region VSTO generated code
+
+        private void InternalStartup()
+        {
+            this.Startup += new System.EventHandler(ThisAddIn_Startup);
+            this.Shutdown += new System.EventHandler(ThisAddIn_Shutdown);
+        }
+
+        #endregion
+    }
+
+}
