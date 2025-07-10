9de7e8c37e5db538565956e8f7c7950aa0118250
m00291 <62349178+m00291@users.noreply.github.com>
2025-07-10 12:49:16 +0800

    use Material Symbols font as arrows
    Added MaterialSymbolsOutlined.ttf to the project. use Material Symbols icons for arrows if the font is available, falling back to text arrows otherwise.

--- SlideNavigationPane.cs
+++ SlideNavigationPane.cs
@@ -1,4 +1,8 @@
 using System;
+using System.Drawing;
+using System.Drawing.Text;
+using System.IO;
+using System.Reflection;
 using System.Windows.Forms;
 
 namespace PowerPointSlideThumbnailsAddIn
@@ -8,9 +12,24 @@ namespace PowerPointSlideThumbnailsAddIn
         public event EventHandler LeftArrowClicked;
         public event EventHandler RightArrowClicked;
 
+        private PrivateFontCollection _privateFonts = new PrivateFontCollection();
+        private bool _fontLoaded = false;
+
         public SlideNavigationPane()
         {
+            LoadMaterialIconsFont(); // Load font before InitializeComponent
             InitializeComponent();
+            this.BackColor = SystemColors.Control;
+        }
+
+        private void LoadMaterialIconsFont()
+        {
+            string fontPath = Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "Fonts", "MaterialSymbolsOutlined.ttf");
+            if (File.Exists(fontPath))
+            {
+                _privateFonts.AddFontFile(fontPath);
+                _fontLoaded = true;
+            }
         }
 
         private void InitializeComponent()
@@ -20,20 +39,36 @@ namespace PowerPointSlideThumbnailsAddIn
             // 
             // btnLeft
             // 
-            this.btnLeft.Font = new System.Drawing.Font("Courier New", 36F, System.Drawing.FontStyle.Bold);
-            this.btnLeft.Text = "<";
+            if (_fontLoaded)
+            {
+                this.btnLeft.Font = new Font(_privateFonts.Families[0], 40, FontStyle.Regular, GraphicsUnit.Point);
+                this.btnLeft.Text = "\ue5de"; // Material Icons: arrow_left
+            }
+            else
+            {
+                this.btnLeft.Font = new Font("Courier New", 36F, FontStyle.Bold);
+                this.btnLeft.Text = "<";
+            }
             this.btnLeft.Width = 120;
-            this.btnLeft.Height = 120;
+            this.btnLeft.Height = 80;
             this.btnLeft.Left = 10;
             this.btnLeft.Top = 10;
             this.btnLeft.Click += (s, e) => LeftArrowClicked?.Invoke(this, EventArgs.Empty);
             // 
             // btnRight
             // 
-            this.btnRight.Font = new System.Drawing.Font("Courier New", 36F, System.Drawing.FontStyle.Bold);
-            this.btnRight.Text = ">";
+            if (_fontLoaded)
+            {
+                this.btnRight.Font = new Font(_privateFonts.Families[0], 40, FontStyle.Regular, GraphicsUnit.Point);
+                this.btnRight.Text = "\ue5df"; // Material Icons: arrow_right
+            }
+            else
+            {
+                this.btnRight.Font = new Font("Courier New", 36F, FontStyle.Bold);
+                this.btnRight.Text = ">";
+            }
             this.btnRight.Width = 120;
-            this.btnRight.Height = 120;
+            this.btnRight.Height = 80;
             this.btnRight.Left = 150;
             this.btnRight.Top = 10;
             this.btnRight.Click += (s, e) => RightArrowClicked?.Invoke(this, EventArgs.Empty);
@@ -43,7 +78,7 @@ namespace PowerPointSlideThumbnailsAddIn
             this.Controls.Add(this.btnLeft);
             this.Controls.Add(this.btnRight);
             this.Width = 290;
-            this.Height = 140;
+            this.Height = 110; // Reduce height to fit in 320px task pane
         }
 
         private Button btnLeft;
