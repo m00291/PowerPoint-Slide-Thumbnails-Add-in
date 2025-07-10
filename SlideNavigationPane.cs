b18cc9492fc2d8cd5a40bb950d73c0fea06f2f41
m00291 <62349178+m00291@users.noreply.github.com>
2025-07-10 14:26:45 +0800

    Add 'End' button and separator to navigation pane

--- SlideNavigationPane.cs
+++ SlideNavigationPane.cs
@@ -14,6 +14,11 @@ namespace PowerPointSlideThumbnailsAddIn
 
         private PrivateFontCollection _privateFonts = new PrivateFontCollection();
         private bool _fontLoaded = false;
+        private Button btnLeft;
+        private Button btnRight;
+        private Button btnEnd;
+        private Label lblEnd;
+        private Panel linePanel;
 
         public SlideNavigationPane()
         {
@@ -36,6 +41,9 @@ namespace PowerPointSlideThumbnailsAddIn
         {
             this.btnLeft = new Button();
             this.btnRight = new Button();
+            this.btnEnd = new Button();
+            this.lblEnd = new Label();
+            this.linePanel = new Panel();
             // 
             // btnLeft
             // 
@@ -53,6 +61,7 @@ namespace PowerPointSlideThumbnailsAddIn
             this.btnLeft.Height = 80;
             this.btnLeft.Left = 10;
             this.btnLeft.Top = 10;
+            this.btnLeft.TextAlign = ContentAlignment.MiddleCenter;
             this.btnLeft.Click += (s, e) => LeftArrowClicked?.Invoke(this, EventArgs.Empty);
             // 
             // btnRight
@@ -71,17 +80,59 @@ namespace PowerPointSlideThumbnailsAddIn
             this.btnRight.Height = 80;
             this.btnRight.Left = 150;
             this.btnRight.Top = 10;
+            this.btnRight.TextAlign = ContentAlignment.MiddleCenter;
             this.btnRight.Click += (s, e) => RightArrowClicked?.Invoke(this, EventArgs.Empty);
+            // 
+            // linePanel
+            // 
+            this.linePanel.Height = 2;
+            this.linePanel.Width = 270;
+            this.linePanel.Left = 10;
+            this.linePanel.Top = 100;
+            this.linePanel.BackColor = Color.LightGray;
+            // 
+            // btnEnd
+            // 
+            if (_fontLoaded)
+            {
+                this.btnEnd.Font = new Font(_privateFonts.Families[0], 36, FontStyle.Regular, GraphicsUnit.Point);
+                this.btnEnd.Text = "\uf2f6"; // Material Icons: cancel_presentation
+            }
+            else
+            {
+                this.btnEnd.Font = new Font("Calibri ", 20F, FontStyle.Bold);
+                this.btnEnd.Text = "End";
+            }
+            this.btnEnd.Width = 120;
+            this.btnEnd.Height = 80;
+            this.btnEnd.Left = 150;
+            this.btnEnd.Top = 110;
+            this.btnEnd.TextAlign = ContentAlignment.MiddleCenter;
+            this.btnEnd.FlatStyle = FlatStyle.Flat;
+            this.btnEnd.FlatAppearance.BorderSize = 0;
+            // 
+            // lblEnd (only if font loaded)
+            // 
+            if (_fontLoaded)
+            {
+                this.lblEnd.Text = "End";
+                this.lblEnd.Font = new Font("Calibri ", 14F, FontStyle.Regular);
+                this.lblEnd.Width = 120;
+                this.lblEnd.Height = 30;
+                this.lblEnd.Left = 150;
+                this.lblEnd.Top = 190;
+                this.lblEnd.TextAlign = ContentAlignment.TopCenter;
+                this.Controls.Add(this.lblEnd);
+            }
             // 
             // SlideNavigationPane
             // 
             this.Controls.Add(this.btnLeft);
             this.Controls.Add(this.btnRight);
+            this.Controls.Add(this.linePanel);
+            this.Controls.Add(this.btnEnd);
             this.Width = 290;
-            this.Height = 110; // Reduce height to fit in 320px task pane
+            this.Height = _fontLoaded ? 230 : 210;
         }
-
-        private Button btnLeft;
-        private Button btnRight;
     }
 }
